e92a5cca83763810e842f54dc1b927b34a55de98
513clean <Zahi.moouess@gmail.com>
2021-12-15 14:40:49 +0200

    teams

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
-import Home from "./components/Home"
+import SelectedTeam from "./components/SelectedTeam"
 import About from "./components/About"
 import Login from "./components/Login"
 import Register from "./components/Register"
@@ -12,8 +12,8 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="about" element={<About />} />
         <Route path="register" element={<Register />} />
+        <Route path="SelectedTeam" element={<SelectedTeam />} />
       </Routes></div>
   );
 }
