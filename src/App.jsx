0823a3b8cb0769685dea7b21557b1dc965acc920
“Zahi <zahi.moouess@gmail.com>
2021-12-19 12:39:12 +0200

    games

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Navbar from "./components/Navbar/Navbar.jsx";
 import SelectedTeam from "./components/SelectedTeam";
 import Login from "./components/Login";
 import Teams from "./components/Teams";
-import Players from "./components/Players"
+import Players from "./components/Players";
 import SelectedPlayer from "./components/PlayerSelected";
 import { Routes, Route } from "react-router-dom";
-
+import Games from "./components/Games";
 import React from "react";
 
 function App() {
@@ -17,10 +17,23 @@ function App() {
       {showNavbar ? <Navbar /> : null}
       <Routes>
         <Route path="/" element={<Login setShowNavbar={setShowNavbar} />} />
-        <Route path="SelectedTeam" element={<SelectedTeam setShowNavbar={setShowNavbar} />} />
+        <Route
+          path="SelectedTeam"
+          element={<SelectedTeam setShowNavbar={setShowNavbar} />}
+        />
         <Route path="teams" element={<Teams setShowNavbar={setShowNavbar} />} />
-        <Route path="players" element={<Players setShowNavbar={setShowNavbar} />} />
-        <Route path="selectedPlayer" element={<SelectedPlayer setShowNavbar={setShowNavbar} />} />
+        <Route
+          path="players"
+          element={<Players setShowNavbar={setShowNavbar} />}
+        />
+        <Route
+          path="selectedPlayer"
+          element={<SelectedPlayer setShowNavbar={setShowNavbar} />}
+        />
+        <Route
+          path="/games"
+          element={<Games setShowNavbar={setShowNavbar} />}
+        />
       </Routes>
     </>
   );
